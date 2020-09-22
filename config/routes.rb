f1de0b6d7d4f71ef253bae5c962db9c840f91fb1
leobr <leobrionesherrera@gmail.com>
2020-09-21 19:42:28 -0500

    routes for login and logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :user, only: [:create, :show]
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in'
 end
