94c820c9d6ce76569b4ef733295a8d4f34ebb2ae
leobr <leobrionesherrera@gmail.com>
2020-09-22 16:15:34 -0500

    Changed login verification

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
     def create
-        @user = User.find_by(email: session_params[:email])
+        @user = User.find_by(username: session_params[:username])
         if @user && @user.authenticate(session_params[:password])
           login!
           render json: {
@@ -16,17 +16,17 @@ class SessionsController < ApplicationController
     end
 
     def is_logged_in?
-        if logged_in? && current_user
-          render json: {
-            logged_in: true,
-            user: current_user
-          }
-        else
-          render json: {
-            logged_in: false,
-            message: 'no such user'
-          }
-        end
+      if logged_in? && current_user
+        render json: {
+          logged_in: true,
+          user: current_user
+        }
+      else
+        render json: {
+          logged_in: false,
+          message: 'no such user'
+        }
+      end
     end
 
     def destroy
@@ -40,7 +40,7 @@ class SessionsController < ApplicationController
     private
     
     def session_params
-        params.require(:user).permit(:username, :email, :password)
-      end
+      params.require(:user).permit(:username, :email, :password)
     end
 end
+
