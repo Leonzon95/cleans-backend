fcf3f33eb9b67ccafe532c8ad1fe92e2c41d763f
leobr <leobrionesherrera@gmail.com>
2020-10-03 16:25:14 -0500

    Users can create a review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,16 @@ class ReviewsController < ApplicationController
 
     def create
         byebug
+        user = User.all.find_by_id(parmas[:user_id])
+        if user.rating
+            user.rating = (params[:review][:rating].to_i + user.rating) / 2 
+        else
+            user.rating = params[:review][:rating].to_i
+        end
+        if params[:review][:comment].strip != '' && params[:review][:rating] 
+            user.reviews.build(review_params)
+        end
+        user.save
     end
 
     private
